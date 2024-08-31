d76cdce89283f27484869d9608f0d3357e19bbac
Anjany <anjany.pandey06@gmail.com>
2024-09-01 00:46:24 +0530

    error solved

--- Dummy api/src/components/LoadingSpinner.jsx
+++ Dummy api/src/components/LoadingSpinner.jsx
@@ -4,7 +4,11 @@ function LoadingSpinner() {
   return (
     <>
       <div className="text-center spinner">
-        <div className="spinner-border" role="status">
+        <div
+          className="spinner-border"
+          role="status"
+          style={{ width: "3rem", height: "3rem" }}
+        >
           <span className="visually-hidden">Loading...</span>
         </div>
       </div>
